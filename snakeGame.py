9f9791dba7ece8819e9bce472b71c609eacf89cd
Ko <132860793+khruc-sail@users.noreply.github.com>
2023-11-11 13:53:49 -0600

    setting up

--- snakeGame.py
+++ snakeGame.py
@@ -1,14 +1,33 @@
+# =====================================================================
+"""
+COMP 123-1 Final Project
+Minseo Kim, Ko Horiuchi
+"""
+# ----------
 import tkinter as tk
 import random
 
+# =====================================================================
 class CanvasGUI:
 
+    # ---------------------------------------------------------------------
+    # Set up window
+    def __init__(self):
+        self.rootWin = tk.Tk()
+        self.rootWin.title("Snake Game")
+
+        mainCanvas = tk.Canvas(self.rootWin)
 
 
-
-    def go(self):
+    # ---------------------------------------------------------------------
+    # Set up snake
+    def moveSnake(self):
         pass
 
-# ---------------------------------------------------------------------
+
+    def run(self):
+        pass
+
+# =====================================================================
 snakeGUI = CanvasGUI
-snakeGUI.go()
+snakeGUI.run()
